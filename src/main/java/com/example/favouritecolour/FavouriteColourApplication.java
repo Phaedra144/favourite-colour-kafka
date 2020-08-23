802ac39a51d2c669ed9f66de9d28929871a8fad6
bedisz <szilvia.bedi@dpdgroup.com>
2020-08-23 16:24:37 +0200

    do little indenting

--- src/main/java/com/example/favouritecolour/FavouriteColourApplication.java
+++ src/main/java/com/example/favouritecolour/FavouriteColourApplication.java
@@ -40,34 +40,34 @@ public class FavouriteColourApplication implements CommandLineRunner {
 
         //Task 1: send filtered colours as "name" key and "colour" value in KStream to topic: {team-name}-filtered-colours
 
-        // Step 1 - create Kstream based on the input topic "favourite-colour-input"
+            // 1 - create Kstream based on the input topic "favourite-colour-input"
 
 
-                // 1 - make sure that a comma is contained in the value as we will split on it
+            // 2 - make sure that a comma is contained in the value as we will split on it
 
-                // 2 - select a key that will be the user name (lowercase for safety)
+            // 3 - select a key that will be the user name (lowercase for safety)
 
-                // 3 - get the colour from the value (lowercase for safety)
+            // 4 - get the colour from the value (lowercase for safety)
 
-                // 4 - log out what key and value you have so far
+            // 5 - log out what key and value you have so far
 
-                // 5 - filter undesired colours (only green, purple and yellow colours should be sent)
+            // 6 - filter undesired colours (only green, purple and yellow colours should be sent)
 
-        // 6 - send filtered kstream to {team-name}-filtered-colours topic, don't forget the serializers
+            // 7 - send filtered kstream to {team-name}-filtered-colours topic, don't forget the serializers
 
 
         //Task 2: count the filtered colours and send them to topic {team-name}-favourite-colour-output
 
-        // Step 1 - read that {team-name}-filtered-colours topic as a KTable so that updates are read correctly
+            // Step 1 - read that {team-name}-filtered-colours topic as a KTable so that updates are read correctly
 
 
-        // Step 2 - count the occurrences of colours
+            // Step 2 - count the occurrences of colours
 
-                // 1 - group by colour within the KTable
+                    // 1 - group by colour within the KTable
 
-                // 2 - count colours that are grouped
+                    // 2 - count colours that are grouped
 
-        // 3 - output the results to a Kafka topic {team-name}-favourite-colour-output - don't forget the serializers
+                    // 3 - output the results to a Kafka topic {team-name}-favourite-colour-output - don't forget the serializers
 
 
 
